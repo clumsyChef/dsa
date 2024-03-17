58dea3f49c93450d60feafa6f396f3c0bd405a20
Sarthak Jha <sarthakjha94@gmail.com>
2024-03-17 19:59:51 +0530

    longest-conse-seq

--- neetcode-roadmap/js/scripts/array-and-hashing/longestConsecutiveSequence.js
+++ neetcode-roadmap/js/scripts/array-and-hashing/longestConsecutiveSequence.js
@@ -23,10 +23,10 @@ const main = {
 };
 
 const longestConsecutiveSequence = () => {
-	console.log(main.solution_2([100, 4, 200, 1, 3, 2]));
-	console.log(main.solution_2([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
-	console.log(main.solution_2([1, 2, 0, 1]));
-	console.log(main.solution_2([4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3]));
+	console.log(main.solution_1([100, 4, 200, 1, 3, 2]));
+	console.log(main.solution_1([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+	console.log(main.solution_1([1, 2, 0, 1]));
+	console.log(main.solution_1([4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3]));
 };
 
 export default longestConsecutiveSequence;
