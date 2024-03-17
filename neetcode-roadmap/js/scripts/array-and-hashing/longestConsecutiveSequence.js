c9d65da65a4770e3e412b747fe1bfea21a9160c4
Sarthak Jha <sarthakjha94@gmail.com>
2024-03-17 20:17:20 +0530

    longest-conse-seq

--- neetcode-roadmap/js/scripts/array-and-hashing/longestConsecutiveSequence.js
+++ neetcode-roadmap/js/scripts/array-and-hashing/longestConsecutiveSequence.js
@@ -20,13 +20,38 @@ const main = {
 
 		return localArr.length > longestStreak ? localArr.length : longestStreak;
 	},
+
+	solution_2(nums) {
+		let maxScore = 0;
+		const numSet = new Set(nums); /* Time O(N) | Space O(N) */
+
+		for (const num of [...numSet]) {
+			/* Time O(N) */
+			const prevNum = num - 1;
+
+			if (numSet.has(prevNum)) continue; /* Time O(N) */
+
+			let [currNum, score] = [num, 1];
+
+			const isStreak = () => numSet.has(currNum + 1);
+			while (isStreak()) {
+				/* Time O(N) */
+				currNum++;
+				score++;
+			}
+
+			maxScore = Math.max(maxScore, score);
+		}
+
+		return maxScore;
+	},
 };
 
 const longestConsecutiveSequence = () => {
-	console.log(main.solution_1([100, 4, 200, 1, 3, 2]));
-	console.log(main.solution_1([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
-	console.log(main.solution_1([1, 2, 0, 1]));
-	console.log(main.solution_1([4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3]));
+	console.log(main.solution_2([100, 4, 200, 1, 3, 2]));
+	console.log(main.solution_2([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+	console.log(main.solution_2([1, 2, 0, 1]));
+	console.log(main.solution_2([4, 0, -4, -2, 2, 5, 2, 0, -8, -8, -8, -8, -1, 7, 4, 5, 5, -4, 6, 6, -3]));
 };
 
 export default longestConsecutiveSequence;
